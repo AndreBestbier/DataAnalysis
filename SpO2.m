3b8e0d02cda779be6f30b72e60a8b0747bbe5ff1
AndreBestbier <abestbier@gmail.com>
2017-07-28 16:42:53 +0200

    SpO2 analysis addad for first 10
    SpO2 analysis addad for first 10
    Beat dependant window addad

--- SpO2.m
+++ SpO2.m
@@ -2,33 +2,48 @@ clear
 clc
 format short g
 
+load DataIndex.mat
+
+meanSureSign = zeros(length(DataIndex), 1);
+meanSats_meanAbs = zeros(length(DataIndex), 1);
+meanSats_meanAbs_beats = zeros(length(DataIndex), 1);
+meanSats_rms = zeros(length(DataIndex), 1);
+
+errSats_meanAbs = zeros(length(DataIndex), 1);
+errSats_meanAbs_beats = zeros(length(DataIndex), 1);
+errSats_rms = zeros(length(DataIndex), 1);
+
+
 %% Select Data folder
-DataFolders = dir ('*Data*');
-str = {DataFolders.name};
-[folderNum,v] = listdlg('PromptString','Select a data file:','SelectionMode','single','ListString',str);
+%     DataFolders = dir ('*Data*');
+%     str = {DataFolders.name};
+%     [folderNum,v] = listdlg('PromptString','Select a data file:','SelectionMode','single','ListString',str);
+%     fprintf('Data folder: %s\n\n', char(str(folderNum)));
 
-fprintf('Data folder: %s\n', char(str(folderNum)));
-
+for n = 1:length(DataIndex)
+%n=1;
+clear r_rms
+clear r_meanAbs
+clear r_meanAbs_beats
+    
 %% Load data
-FolderName = 'Data4';
-beatText = csvread(strcat(FolderName, '\beatText.txt'),3,0);
+%FolderName = char(str(folderNum));
+%FolderName = '1. Philip1_Data';
+FolderName = DataIndex(n);
 ppgText = csvread(strcat(FolderName, '\ppgText.txt'),3,0);
-EarPeaksMillis = csvread(strcat(FolderName, '\EarPeaksMillis1.txt'),1,0);
-
-EarSats = beatText(:,6);
-time = beatText(:,3);
-time = time-time(1);
+EarPeaksMillis = csvread(strcat(FolderName, '\EarPeaksMillis.txt'),1,0);
 
 ppgMillis = ppgText(:,3);
 rawIR = ppgText(:,4);
 rawRed = ppgText(:,5);
 irFiltered = ppgText(:,6);
 nPPG = length(rawIR);
-X_ppg = 1:nPPG;
+X_ppg = ppgMillis;
 
-%% Calculation
+SureSign = csvread(strcat(FolderName, '\SureSignSats.txt'));
+SureSign_X = (0:1000:119000)' + X_ppg(1);
 
-%AC/DC extraction
+%% AC/DC extraction
 alpha = 0.7;               
 irW = 0;
 redW = 0;
@@ -48,17 +63,7 @@ end
 irDC = rawIR - irAC;
 redDC = rawRed - redAC;
 
-%Remove transient strat
-irAC = irAC(60:length(irAC));
-irDC = irDC(60:length(irDC));
-redAC = redAC(60:length(redAC));
-redDC = redDC(60:length(redDC));
-
-X_ppg = X_ppg(60:length(X_ppg));
-irFiltered = irFiltered(60:length(irFiltered));
-ppgMillis = ppgMillis(60:length(ppgMillis));
-
-%Filter
+%% Filter
 [b, a] = butter(3, 3/(50/2), 'low');
 
 irAC_filt = filter(b,a,irAC);
@@ -66,37 +71,8 @@ redAC_filt = filter(b,a,redAC);
 irDC_filt = filter(b,a,irDC);
 redDC_filt = filter(b,a,redDC);
 
-%SpO2 calcs
-r_meanAbs = zeros(length(EarPeaksMillis), 1);
-r_rms = zeros(length(EarPeaksMillis), 1);
-pos = zeros(length(EarPeaksMillis), 1);
-
-for i = 1:length(EarPeaksMillis)
-    pos(i) = find(ppgMillis==EarPeaksMillis(i));
-    
-    if i<=10
-        window = 1:pos(i);
-    else
-        window = pos(i-10):pos(i);
-    end
-    
-    %Mean Abs value
-    irAC_meanAbs = mean(sqrt(irAC_filt(window).^2));
-    redAC_meanAbs = mean(sqrt(redAC_filt(window).^2));
-    irDC_meanAbs = mean(sqrt(irDC_filt(window).^2));
-    redDC_meanAbs = mean(sqrt(redDC_filt(window).^2));
-    
-    %RMS value
-    irAC_rms = rms(irAC_filt(window));
-    redAC_rms = rms(redAC_filt(window));
-    irDC_rms = rms(irDC_filt(window));
-    redDC_rms = rms(redDC_filt(window));
-        
-    r_meanAbs(i) = (redAC_meanAbs/redDC_meanAbs)/(irAC_meanAbs/irDC_meanAbs);
-    r_rms(i) = (redAC_rms/redDC_rms)/(irAC_rms/irDC_rms);
-end
-
-windowSize = 500;
+%% Fixed window
+windowSize = 50;
 r = zeros(length(ppgMillis)-windowSize+1, 1);
 
 for i=windowSize+1:length(ppgMillis)
@@ -105,37 +81,88 @@ for i=windowSize+1:length(ppgMillis)
     irDC_rms = rms(irDC_filt(i-windowSize:i));
     redDC_rms = rms(redDC_filt(i-windowSize:i));
     
-    r(i) = (redAC_rms/redDC_rms)/(irAC_rms/irDC_rms);
+    r_rms(i) = (redAC_rms/redDC_rms)/(irAC_rms/irDC_rms);
+    
+    irAC_meanAbs = mean(abs(irAC_filt(i-windowSize:i)));
+    redAC_meanAbs = mean(abs(redAC_filt(i-windowSize:i)));
+    irDC_meanAbs = mean(abs(irDC_filt(i-windowSize:i)));
+    redDC_meanAbs = mean(abs(redDC_filt(i-windowSize:i)));
+    
+    r_meanAbs(i) = (redAC_meanAbs/redDC_meanAbs)/(irAC_meanAbs/irDC_meanAbs);
 end
-r = r(windowSize+1:length(r));
-Sats_500 = 113-(25*r);
+Sats_meanAbs = 111.2-(25*r_meanAbs);
+Sats_rms = 111.51-(25*r_rms);
+
+%% Beat dependant window
+numBeats = 12;
+
+peakIndex = zeros(length(EarPeaksMillis)+1, 1);
+peakIndex(1) = 1;
+for i=2:length(EarPeaksMillis)
+peakIndex(i) = find(X_ppg == EarPeaksMillis(i));
+end
+
+for i=1:length(EarPeaksMillis)-numBeats
+    irAC_meanAbs = mean(abs(irAC_filt(peakIndex(i):peakIndex(i+numBeats))));
+    redAC_meanAbs = mean(abs(redAC_filt(peakIndex(i):peakIndex(i+numBeats))));
+    irDC_meanAbs = mean(abs(irDC_filt(peakIndex(i):peakIndex(i+numBeats))));
+    redDC_meanAbs = mean(abs(redDC_filt(peakIndex(i):peakIndex(i+numBeats))));
+
+    r_meanAbs_beats(i) = (redAC_meanAbs/redDC_meanAbs)/(irAC_meanAbs/irDC_meanAbs);
+end
+
+Sats_meanAbs_beats = 111.2-(25*r_meanAbs_beats);
+
+%%Remove transients
+SureSign = SureSign(12:length(SureSign));
+SureSign_X = SureSign_X(12:length(SureSign_X));
+
+Sats_meanAbs = Sats_meanAbs(600:length(Sats_meanAbs));
+Sats_rms = Sats_rms(600:length(Sats_rms));
+X_ppg = X_ppg(600:length(X_ppg));
+
+irAC_filt = irAC_filt(600:length(irAC_filt));
+redAC_filt = redAC_filt(600:length(redAC_filt));
+
+Sats_meanAbs_beats = Sats_meanAbs_beats(3:length(Sats_meanAbs_beats));
+EarPeaksMillis_X = EarPeaksMillis(numBeats+2:length(EarPeaksMillis));
 
 
-Sats_meanAbs = 113-(25*r_meanAbs);
-Sats_rms = 113-(25*r_rms);
+%% Results
+meanSureSign(n) = mean(SureSign);
+meanSats_meanAbs(n) = mean(Sats_meanAbs);
+meanSats_meanAbs_beats(n) = mean(Sats_meanAbs_beats);
+meanSats_rms(n) = mean(Sats_rms);
+
+errSats_meanAbs(n) =  meanSureSign(n)-meanSats_meanAbs(n);
+errSats_meanAbs_beats(n) =  meanSureSign(n)-meanSats_meanAbs_beats(n);
+
+errSats_rms(n) =  meanSureSign(n)-meanSats_rms(n);
 
 %% Plot
-figure();
-plot(1:length(EarSats), EarSats); hold on;
-plot(1:length(Sats_meanAbs), Sats_meanAbs);
-plot(1:length(Sats_rms), Sats_rms);
-plot((1:length(Sats_500))./43, Sats_500);
-legend('Ear-Monitor Sats','MATLAB Sats_meanAbs', 'MATLAB Sats_rms', 'Sats_500'); hold off;
+figure('name',FolderName, 'units','normalized','outerposition',[0 0 1 1]);
+subplot(2,1,1)
+plot(SureSign_X, SureSign, 'Color',[178/255 48/255 48/255]); hold on;
+plot(X_ppg, Sats_meanAbs, 'Color',[121/255 178/255 196/255]);
+plot(EarPeaksMillis_X, Sats_meanAbs_beats, 'Color',[56/255 52/255 173/255]);
+legend('SureSign Sats', 'MATLAB Sats-meanAbs', 'MATLAB Sats-meanAbs-beats'); hold off;
 
-figure();
-plot(ppgMillis, irAC_filt, ppgMillis, redAC_filt);
-legend('irAC_filt','redAC_filt');
-
-% figure();
-% plot(ppgMillis, irAC_filt, ppgMillis, irFiltered); hold on;
-% plot(EarPeaksMillis, 20, 'o', 'Color',[1 0 0], 'MarkerSize', 3, 'MarkerFaceColor', [1 0 0]);
-% title('Filtered IR generated by this Sketch and Filtered IR saved by Ear-Monitor. Beats marked generated by MATLAB beat detection');
-% legend('irAC','irFiltered'); hold off;
+subplot(2,1,2)
+plot(X_ppg, irAC_filt); hold on;
+plot(X_ppg, redAC_filt);
+plot(EarPeaksMillis, 50, 'o', 'Color',[1 0 0], 'MarkerSize', 3, 'MarkerFaceColor', [1 0 0]);
+legend('IR AC','Red AC'); hold off;
 
 
 %% Print Results
-fprintf('Total SpO2 = %f\n\n', mean(EarSats));
-fprintf('Average MATLAB Sats_meanAbs = %f\n\n', mean(Sats_meanAbs));
-fprintf('Average MATLAB Sats_rms = %f\n\n', mean(Sats_rms));
-fprintf('Average MATLAB Sats_500 = %f\n\n', mean(Sats_500));
+fprintf('Data folder: %s\n\n', FolderName);
+fprintf('Average SureSign \t\t\t\t\t= %f\n', meanSureSign(n));
+fprintf('Average MATLAB Sats_meanAbs \t\t= %f \tSTD: %f\n', meanSats_meanAbs(n), std(Sats_meanAbs));
+fprintf('Average MATLAB Sats_rms \t\t\t= %f \tSTD: %f\n', meanSats_rms(n), std(Sats_rms));
+fprintf('Error between SureSign and Sats_meanAbs\t\t\t= %f\n', errSats_meanAbs(n));
+fprintf('Error between SureSign and Sats_meanAbs_beats\t= %f\n', errSats_meanAbs_beats(n));
 
+end
+
+[errSats_meanAbs errSats_meanAbs_beats errSats_rms]
+[mean(errSats_meanAbs) mean(errSats_meanAbs_beats) mean(errSats_rms)]
